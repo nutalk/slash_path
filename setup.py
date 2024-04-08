3cc4333749939d85db39108f7b6f8a7031c4b929
Tao He <tao.he@mixosense.com>
2024-04-08 11:06:18 +0800

    Adjust readme

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("readme.rst", "r", encoding='utf-8') as fh:
+with open("readme.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 setup(
     name="slash_path",
-    version="0.1.0",
+    version="0.1.1",
     author="Nutalk",
     author_email="ht2005112@hotmail.com",
     description="use windows path to generate Path object.",
@@ -13,16 +13,13 @@ setup(
     url="https://github.com/nutalk/slash_path",
     packages=find_packages(),
     long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         'Programming Language :: Python :: Implementation :: CPython',
     ],
 )
